33e9b45deb4012447dc94744a0b40aef926aaddf
wrongholt <wrongholt@gmail.com>
2023-05-11 20:06:04 -0500

    1 binaries uploaded

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -1,119 +1,119 @@
-import WebFontFile from "../helpers/webfont.js";
+import WebFontFile from '../helpers/webfont.js';
 class BootScene extends Phaser.Scene {
   constructor(args) {
     super();
   }
 
   preload() {
-    var arrayVideo = ["streets", "sky"];
+    var arrayVideo = ['streets', 'sky'];
     var pickOne = arrayVideo[Math.floor(Math.random() * arrayVideo.length)];
-    this.load.crossOrigin = "Anonymous";
+    this.load.crossOrigin = 'Anonymous';
     this.load.addFile(
-      new WebFontFile(this.load, ["Sedgwick Ave Display", "Lato", "Caveat"])
+      new WebFontFile(this.load, ['Sedgwick Ave Display', 'Lato', 'Caveat'])
     );
     this.load.video(
-      "background",
-      "https://d1ofwchetll7ui.cloudfront.net/" + pickOne + ".mp4"
+      'background',
+      'https://d1ofwchetll7ui.cloudfront.net/' + pickOne + '.mp4'
     );
     this.load.image(
-      "logo",
-      "https://d1ofwchetll7ui.cloudfront.net/powerslogo.png"
+      'logo',
+      'https://d1ofwchetll7ui.cloudfront.net/powerslogo.png'
     );
     this.load.image(
-      "button",
-      "https://d1ofwchetll7ui.cloudfront.net/button.png"
+      'button',
+      'https://d1ofwchetll7ui.cloudfront.net/button.png'
     );
 
     this.load.image(
-      "button_hover",
-      "https://d1ofwchetll7ui.cloudfront.net/button_hover.png"
+      'button_hover',
+      'https://d1ofwchetll7ui.cloudfront.net/button_hover.png'
     );
-    this.load.image("CharityAvatar", "assets/CharityAvatar.png");
-    this.load.image("ArgusAvatar", "assets/ArgusAvatar.png");
-    this.load.image("CaptainNemoAvatar", "assets/CaptainNemoAvatar.png");
-    this.load.image("DejahThorisAvatar", "assets/DejahThorisAvatar.png");
-    this.load.image("EdgeAvatar", "assets/EdgeAvatar.png");
-    this.load.image("ElectricMeanAvatar", "assets/ElectricMeanAvatar.png");
-    this.load.image("ElkeemaAvatar", "assets/ElkeemaAvatar.png");
-    this.load.image("JohnCarterAvatar", "assets/JohnCarterAvatar.png");
-    this.load.image("KarriganAvatar", "assets/KarriganAvatar.png");
+    this.load.image('CharityAvatar', 'assets/CharityAvatar.png');
+    this.load.image('ArgusAvatar', 'assets/ArgusAvatar.png');
+    this.load.image('CaptainNemoAvatar', 'assets/CaptainNemoAvatar.png');
+    this.load.image('DejahThorisAvatar', 'assets/DejahThorisAvatar.png');
+    this.load.image('EdgeAvatar', 'assets/EdgeAvatar.png');
+    this.load.image('ElectricMeanAvatar', 'assets/ElectricMeanAvatar.png');
+    this.load.image('ElkeemaAvatar', 'assets/ElkeemaAvatar.png');
+    this.load.image('JohnCarterAvatar', 'assets/JohnCarterAvatar.png');
+    this.load.image('KarriganAvatar', 'assets/KarriganAvatar.png');
     this.load.image(
-      "LarsThundersquatAvatar",
-      "assets/LarsThundersquatAvatar.png"
+      'LarsThundersquatAvatar',
+      'assets/LarsThundersquatAvatar.png'
     );
-    this.load.image("LillithAvatar", "assets/LillithAvatar.png");
-    this.load.image("RandellAvatar", "assets/RandellAvatar.png");
-    this.load.image("SharpySharpAvatar", "assets/SharpySharpAvatar.png");
+    this.load.image('LillithAvatar', 'assets/LillithAvatar.png');
+    this.load.image('RandellAvatar', 'assets/RandellAvatar.png');
+    this.load.image('SharpySharpAvatar', 'assets/SharpySharpAvatar.png');
 
     this.load.multiatlas(
-      "Charity",
-      "https://d1ofwchetll7ui.cloudfront.net/Charity.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'Charity',
+      'https://d1ofwchetll7ui.cloudfront.net/Charity.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "Argus",
-      "https://d1ofwchetll7ui.cloudfront.net/Argus.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'Argus',
+      'https://d1ofwchetll7ui.cloudfront.net/Argus.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "CaptainNemo",
-      "https://d1ofwchetll7ui.cloudfront.net/CaptainNemo.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'CaptainNemo',
+      'https://d1ofwchetll7ui.cloudfront.net/CaptainNemo.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "DejahThoris",
-      "https://d1ofwchetll7ui.cloudfront.net/DejahThoris.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'DejahThoris',
+      'https://d1ofwchetll7ui.cloudfront.net/DejahThoris.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "Edge",
-      "https://d1ofwchetll7ui.cloudfront.net/Edge.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'Edge',
+      'https://d1ofwchetll7ui.cloudfront.net/Edge.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "ElectricMean",
-      "https://d1ofwchetll7ui.cloudfront.net/ElectricMean.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'ElectricMean',
+      'https://d1ofwchetll7ui.cloudfront.net/ElectricMean.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "Elkeema",
-      "https://d1ofwchetll7ui.cloudfront.net/Elkeema.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'Elkeema',
+      'https://d1ofwchetll7ui.cloudfront.net/Elkeema.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "JohnCarter",
-      "https://d1ofwchetll7ui.cloudfront.net/JohnCarter.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'JohnCarter',
+      'https://d1ofwchetll7ui.cloudfront.net/JohnCarter.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "Karrigan",
-      "https://d1ofwchetll7ui.cloudfront.net/Karrigan.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'Karrigan',
+      'https://d1ofwchetll7ui.cloudfront.net/Karrigan.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "LarsThundersquat",
-      "https://d1ofwchetll7ui.cloudfront.net/LarsThundersquat.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'LarsThundersquat',
+      'https://d1ofwchetll7ui.cloudfront.net/LarsThundersquat.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "Lillith",
-      "https://d1ofwchetll7ui.cloudfront.net/Lillith.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'Lillith',
+      'https://d1ofwchetll7ui.cloudfront.net/Lillith.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "Randell",
-      "https://d1ofwchetll7ui.cloudfront.net/Randell.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'Randell',
+      'https://d1ofwchetll7ui.cloudfront.net/Randell.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
     this.load.multiatlas(
-      "SharpySharp",
-      "https://d1ofwchetll7ui.cloudfront.net/SharpySharp.json",
-      "https://d1ofwchetll7ui.cloudfront.net/"
+      'SharpySharp',
+      'https://d1ofwchetll7ui.cloudfront.net/SharpySharp.json',
+      'https://d1ofwchetll7ui.cloudfront.net/'
     );
   }
 
   create() {
-    this.scene.start("HomeScene");
+    this.scene.start('HomeScene');
   }
 }
 
--- src/scenes/SceneSelectionScene.js
+++ src/scenes/SceneSelectionScene.js
@@ -1,111 +1,62 @@
-// class SceneSelectionScene extends Phaser.Scene {
-//   constructor(args) {
-//     super();
-//   }
-//   preload() {}
-//   create() {
-//     var newHeight = this.game.config.height;
-//     var newWidth = this.game.config.width;
-//     let background = this.add.video(0, 0, 'background');
-//     background.setOrigin(0, 0);
-//     background.setSize(newWidth, newHeight);
-//     background.setScale(newWidth / 1080);
-//     background.play(true);
-//     // Add 6 images to the spinning scene
-// for (var i = 0; i < 6; i++) {
-//   var image = new Phaser.Sprite({
-//     key: 'image' + i,
-//     x: 0,
-//     y: 0,
-//     src: 'assets/images/image' + i + '.png'
-//   });
+class SceneSelectionScene extends Phaser.Scene {
+  constructor(args) {
+    super();
+  }
+  preload() {}
+  create() {
+    var newHeight = this.game.config.height;
+    var newWidth = this.game.config.width;
+    let background = this.add.video(0, 0, 'background');
+    background.setOrigin(0, 0);
+    background.setSize(newWidth, newHeight);
+    background.setScale(newWidth / 1080);
+    background.play(true);
+    var scene1 = this.add
+      .image(newWidth / 2, newHeight / 2, 'scene1')
+      .setOrigin(0.5);
 
-//   spinningScene.add(image);
-// }
+    var r = Math.floor(Math.random() * 20);
+    var image = `scene${r}`;
+    var i = 2;
+    var tween = this.tweens.add({
+      targets: scene1,
+      props: {
+        scaleX: { value: 0, duration: 100, yoyo: true },
+        texture: {
+          value: 'scene' + [Math.floor(Math.random() * 4) + 1],
+          duration: 0,
+          delay: 1500,
+        },
+      },
+      repeat: r,
+      onRepeat: (tween, target) => {
+        var texture = 'scene' + [Math.floor(Math.random() * 4) + 1];
+        target.setTexture(texture);
+        console.log('TEXTURES', this.texture);
+        // debugger;
+      },
+      ease: 'Linear',
+      onComplete: (tween, targets, param) => {
+        scene1.setTexture(targets[0].texture.key);
+        this.newTexture = targets[0].texture.key;
+        this.tweens.add({
+          targets: scene1,
 
-// // Add a rotation tween to each image
-// for (var i = 0; i < 6; i++) {
-//   var image = spinningScene.get(i);
-//   var tween = image.tween({
-//     angle: 0,
-//     angleMax: 360,
-//     duration: Math.random() * 1000
-//   });
-
-//   tween.onComplete.add(function() {
-//     // When the tween completes, select the image
-//     var selectedImage = image;
-//     console.log('The selected image is', selectedImage.key);
-//   });
-// }
-// var timer = new Phaser.Timer();
-
-// // Start the timer
-// timer.start(function() {
-//   // Rotate each image by a random amount
-//   for (var i = 0; i < 6; i++) {
-//     images[i].rotation += Math.random() * 360;
-//   }
-
-//   // Check if the timer has reached its maximum value
-//   if (timer.time >= 1000) {
-//     // Stop the timer
-//     timer.stop();
-
-//     // Select the image that is currently facing the camera
-//     var selectedImage = images[0];
-
-//     for (var i = 1; i < 6; i++) {
-//       if (images[i].rotation < selectedImage.rotation) {
-//         selectedImage = images[i];
-//       }
-//     }
-
-//     // Do something with the selected image
-//     console.log('The selected image is:', selectedImage.key);
-//   }
-// });
-// this.spinningGroup = this.add.group();
-
-// // Create six spinning sprites and add them to the group
-// for (let i = 0; i < 3; i++) {
-//     let xPos = Phaser.Math.Between(100, 500);
-//     let yPos = Phaser.Math.Between(100, 500);
-//     let imageKey = 'front' + (i + 1);
-
-//     let spinningSprite = this.add.sprite(xPos, yPos, imageKey);
-//     spinningSprite.setOrigin(0.5);
-//     spinningSprite.setDepth(i);
-//     spinningSprite.spinSpeed = Phaser.Math.Between(1, 5);
-//     this.spinningGroup.add(spinningSprite);
-// }
-
-// // Call the spin method to rotate the group
-// this.spin();
-// spin() {
-//   // Rotate the group of spinning sprites for a random amount of time
-//   let spinTime = Phaser.Math.Between(2000, 4000);
-//   let spinTween = this.tweens.add({
-//       targets: this.spinningGroup.getChildren(),
-//       scaleX: { value: 0, duration: 1000, yoyo: true },
-//                 texture: { value: 'front', duration: 0, delay: 1000 },
-//       angle: 360,
-//       duration: spinTime,
-//       repeat: 0,
-//       ease: 'Linear',
-//       onComplete: () => {
-//           // Select a random spinning sprite to stop on
-//           let selectedIndex = Phaser.Math.Between(0, 5);
-//           let selectedSprite = this.spinningGroup.getChildren()[selectedIndex];
-
-//           // Change the image of the selected sprite
-//           // selectedSprite.setTexture('back');
-//           console.log(selectedSprite);
-//           // Call the spin method again to start a new spin
-//           this.spin();
-//       }
-//   });
-// }
-//   }
-// }
-// export default SceneSelectionScene;
+          scaleX: 3,
+          scaleY: 3,
+        });
+        // debugger;
+      },
+    });
+    tween.on('repeat', function () {
+      this.texture = 'scene' + [Math.floor(Math.random() * 5)];
+      if (i < 5) {
+        i++;
+      } else {
+        i = 1;
+      }
+    });
+    console.log('hello');
+  }
+}
+export default SceneSelectionScene;
